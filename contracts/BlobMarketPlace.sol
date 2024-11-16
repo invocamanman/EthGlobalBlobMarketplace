acb01784828cdb45d7484656ab9816b90bc899d0
invocamanman <jesus.ligero@yahoo.es>
2024-11-16 08:01:14 +0100

    first commit

---
+++ contracts/BlobMarketPlace.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: AGPL-3.0
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+/**
+ * Contract responsible for managing Blob marketPLace
+**/
+contract BlobMarketPlace is
+    Ownable
+{
+     //using SafeERC20 for IERC20;
+    constant uint256 minimalFee = 0.5 ethers
+    
+
+    constructor() {
+
+    }
+
+
+}
